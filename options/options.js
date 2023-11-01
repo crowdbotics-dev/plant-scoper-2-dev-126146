5a7c48f7a318b7e98d3c7ab975c8050f047d76c9
Crowdbotics <engineering@crowdbotics.com>
2023-11-01 22:33:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "plant-scoper-2-dev-126146",
+  url: "https://plant-scoper-2-dev-126146.botics.co",
+  api: "https://plant-scoper-2-dev-126146.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
